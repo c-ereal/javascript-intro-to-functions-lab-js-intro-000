09a3434b220fe8d688cae10442b6683a495a6c15
bere abreo <berenicej2013@gmail.com>
2021-04-02 04:05:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!";
   } else if (string.toLowerCase() === lowercase) {
     return "I can't hear you!";
-  } else (string.toLowerCase() === mixedCase || string.toUpperCase() === mixedCase) {
+  } else {
     return "I love you, too."
   }
 }
